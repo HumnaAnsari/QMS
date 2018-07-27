8cf3ff3f8c80d2da857fc576ca7104e6c309a4cc
Humna Ansari <humna.ansari@ibexglobal.com>
2018-07-27 17:56:02 +0500

    commit

---
+++ QuizManagementSystem/ExpenseTrackerApp/Controllers/HomeController.cs
@@ -0,0 +1,534 @@
+﻿using ExpenseTrackerApp.Models;
+
+using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using static ExpenseTrackerApp.Models.DAL;
+
+namespace ExpenseTrackerApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILog logger = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        public ActionResult AdminView()
+        {
+            return View();
+        }
+
+        public ActionResult AgentView()
+        {
+            return View();
+        }
+
+        public ActionResult CreateQuestions()
+        {
+            return View();
+        }
+        public ActionResult CreateSubjects()
+        {
+            return View();
+        }
+
+        public ActionResult CreateQuiz()
+        {
+            return View();
+        }
+        public ActionResult Help()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult ViewQuiz()
+        {
+            return View();
+        }
+        public ActionResult AddUser(AddUserModel model)
+        {
+            try
+            {
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    var result = db.addUser(model.UserName, model.FirstName, model.LastName, model.Password, model.Email, model.PhoneNumber, model.SecretQuestion,model.SecretAnswer, model.Role);
+                    return Json(result, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return null;
+        }
+
+        public ActionResult EditUser(AddUserModel model)
+        {
+            try
+            {
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    var result = db.editUser(model.UserId,model.UserName, model.FirstName, model.LastName, model.Password, model.Email, model.PhoneNumber, model.SecretQuestion, model.SecretAnswer, model.RoleName);
+                    return Json(result, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+            //return null;
+        }
+
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        public ActionResult LeftPanel()
+        {
+            return PartialView("~/Views/Shared/_LeftPanel.cshtml");
+        }
+        public ActionResult Get_ExpenseDataType()
+        {
+            return null;
+        }
+        
+
+        public ActionResult DeleteUser(int UserId)
+        {
+            try
+            {
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    var data = db.deleteUser(UserId);
+                    return Json(data, JsonRequestBehavior.AllowGet);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            return null;
+        }
+
+        public ActionResult DeleteQuiz(int QuizId)
+        {
+            try
+            {
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    var data = db.deleteQuiz(QuizId);
+                    return Json(data, JsonRequestBehavior.AllowGet);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            return null;
+        }
+
+        public ActionResult AssignQuiz(AssignQuizModel model)
+        {
+            try
+            {
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    var data = db.assignQuizToAgent(model.QuizId,model.TeacherId,model.ExpireDate,model.Agents);
+                    return Json(data, JsonRequestBehavior.AllowGet);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            return null;
+        }
+        public ActionResult AgentGridData(int uid)
+        {
+            try
+            {
+                List<getAgentGridData_Result> data = new List<getAgentGridData_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    data = db.getAgentGridData(uid).ToList();
+                }
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                // logger.Error(ex);
+            }
+
+            return null;
+
+        }
+
+        public ActionResult ListUsers()
+        {
+            try
+            {
+                List<getUserDetails_Result> data = new List<getUserDetails_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    data = db.getUserDetails().ToList();
+                }
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                // logger.Error(ex);
+            }
+
+            return null;
+
+        }
+        
+        public ActionResult GetTestCategories()
+        {
+            try
+            {
+                List<getTestCategories_Result> data = new List<getTestCategories_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    data = db.getTestCategories().ToList();
+                }
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                // logger.Error(ex);
+            }
+
+            return null;
+
+        }
+
+        public ActionResult ListSubjects()
+        {
+            try
+            {
+                List<getSubjectsList_Result> data = new List<getSubjectsList_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    data = db.getSubjectsList().ToList();
+                    foreach (var d in data)
+                    {
+
+                    }
+                }
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                // logger.Error(ex);
+            }
+
+            return null;
+
+        }
+        public ActionResult AddSubject(AddSubjectModel model)
+        {
+            try
+            {
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    var result = db.addSubject(model.SubjectName,model.Category,model.ActiveF);
+                    return Json(result, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return null;
+        }
+
+        public ActionResult ListQuestions()
+        {
+            try
+            {
+                List<getQuestionList_Result> data = new List<getQuestionList_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    data = db.getQuestionList().ToList();
+                }
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                // logger.Error(ex);
+            }
+
+            return null;
+
+        }
+
+        public ActionResult AddQuestion(AddQuestionModel model)
+        {
+            try
+            {
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    var result = db.addQuestion(model.QuestionType,model.Category,model.Question,model.OptionA,model.OptionB,model.OptionC,model.OptionD,model.Answer);
+                    return Json(result, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch (Exception e)
+            {
+
+            }
+            return null;
+        }
+
+        public ActionResult DeleteQuestion(int QId)
+        {
+            try
+            {
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    var data = db.deleteQuestion(QId);
+                    return Json(data, JsonRequestBehavior.AllowGet);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            return null;
+        }
+
+        public ActionResult GetSubjects(string catid)
+        {
+            try
+            {
+                int cid = Convert.ToInt32(catid);
+                List<getSubjects_Result> data = new List<getSubjects_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    data = db.getSubjects(cid).ToList();
+                }
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                // logger.Error(ex);
+            }
+
+            return null;
+
+        }
+
+        public ActionResult GetAgents()
+        {
+            try
+            {
+                
+                List<getAgents_Result> data = new List<getAgents_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    data = db.getAgents().ToList();
+                }
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                // logger.Error(ex);
+            }
+
+            return null;
+
+        }
+
+        public ActionResult ListQuiz()
+        {
+            try
+            {
+                List<getQuizList_Result> data = new List<getQuizList_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    data = db.getQuizList().ToList();
+                }
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                // logger.Error(ex);
+            }
+
+            return null;
+
+        }
+
+
+        public ActionResult DeleteSubject(int SId)
+        {
+            try
+            {
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    var data = db.deleteSubject(SId);
+                    return Json(data, JsonRequestBehavior.AllowGet);
+                }
+
+
+            }
+            catch (Exception e)
+            {
+
+            }
+            return null;
+        }
+
+
+
+        public ActionResult AddQuiz(AddQuizModel model)
+        {
+            try
+            {
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    var result = db.addQuiz(model.QuizName, model.TotalQuestions, model.TimeAllocated, model.PassingScore, model.Subjects, model.CatID);
+                    return Json(result, JsonRequestBehavior.AllowGet);
+                }
+            }
+            catch(Exception e)
+            {
+
+            }
+            return null;
+        }
+
+        public ActionResult GetSecretQuestion()
+        {
+            try
+            {
+                List<getSecretQuestions_Result> data = new List<getSecretQuestions_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    
+                    data = db.getSecretQuestions().ToList();
+                }
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                //logger.Error(ex);
+                return null;
+            }
+        }
+        public ActionResult GetRoles()
+        {
+            try
+            {
+                List<getRoles_Result> data = new List<getRoles_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    data = db.getRoles().ToList();
+                }
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                //logger.Error(ex);
+                return null;
+            }
+        }
+        /*Sharoz New Work*/
+        public ActionResult GetQuizData_Agent(int uid)
+        {
+            try
+            {
+                List<GetQuizData_Agent_Result> data = new List<GetQuizData_Agent_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    data = db.GetQuizData_Agent(uid).ToList();
+                }
+
+                return Json(data, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                // logger.Error(ex);
+            }
+
+            return null;
+
+        }
+        public ActionResult GetQuizQuestions(int UserId, int QuizID, int TimeAllocated)
+        {
+            var obj = this.DeserializeObject<QuizModel>("QuizModel");
+            ViewBag.TimeAllocated = TimeAllocated.ToString();
+            try
+            { 
+                List<GetQuizQuestions_Result> data = new List<GetQuizQuestions_Result>();
+                using (DBONLINETESTEntities db = new DBONLINETESTEntities())
+                {
+                    data = db.GetQuizQuestions(UserId, QuizID).ToList();
+                }
+               
+                List<QuestionsModel> list = new List<QuestionsModel>();
+                for (int i = 0; i < data.Count(); i++)
+                {
+                    QuestionsModel ques = new QuestionsModel();
+                    ques.Answer = data[i].Answer.ToString();
+                    ques.ChoiceType = data[i].ChoiceType.ToString();
+                    ques.Option1 = data[i].Option1.ToString();
+                    ques.Option2 = data[i].Option2.ToString();
+                    ques.Option3 = data[i].Option3.ToString();
+                    ques.Option4 = data[i].Option4.ToString();
+                    ques.Question = data[i].Question.ToString();
+                    ques.QuizID = Convert.ToInt32(data[i].QuizID.ToString());
+                    ques.SubjectID = Convert.ToInt32(data[i].SubjectID.ToString());
+                    ques.SubjectName = data[i].SubjectName.ToString();
+                    list.Add(ques);
+                }
+               
+                return View("Quiz",list);
+            }
+            catch (Exception ex)
+            {
+                // logger.Error(ex);
+            }
+
+            return null;
+
+        }
+        //public ActionResult Quiz(QuizModel Model)
+        //{
+        //    try
+        //    {
+               
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        throw;
+        //    }
+        //    return null;
+        //}
+        /*Sharoz New Work*/
+
+    }
+}
---
+++ QuizManagementSystem/ExpenseTrackerApp/Models/DAL.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExpenseTrackerApp.Models
+{
+    public class DAL
+    {
+        public class UserModel
+        {
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+            public string Password { get; set; }
+            public int RoleId { get; set; }
+        
+        }
+        public class AddUserModel
+        {
+            public int UserId { get; set; }
+            public string UserName { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int Role { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+            public string SecretQuestion { get; set; }
+            public string SecretAnswer { get; set; }
+            public string Password { get; set; }
+            public int RoleId { get; set; }
+            public string RoleName { get; set; }
+        }
+
+        public class AddQuizModel
+        {
+            public string QuizName { get; set; }
+            public int TotalQuestions { get; set; }
+            public int CatID { get; set; }
+            public int TimeAllocated { get; set; }
+            public int PassingScore { get; set; }
+            public string Subjects { get; set; }
+
+        }
+        public class AddQuestionModel
+        {
+            public string QuestionType { get; set; }
+            public string Category { get; set; }
+            public string Question { get; set; }
+            public string OptionA { get; set; }
+            public string OptionB { get; set; }
+            public string OptionC { get; set; }
+            public string OptionD { get; set; }
+            public string Answer { get; set; }
+
+        }
+        public class AddSubjectModel
+        {
+            public string SubjectName { get; set; }
+            public int ActiveF { get; set; }
+            public string Category { get; set; }
+           
+        }
+
+        public class AssignQuizModel
+        {
+            public int QuizId { get; set; }
+            public string ExpireDate { get; set; }
+            public int TeacherId { get; set; }
+            public string Agents { get; set; }
+
+        }
+
+        public class Expense
+        {
+            public int Amount{ get; set; }
+            public string Comment { get; set; }
+            public int ExpenseDataType { get; set; }
+        }
+        public class UserProfileSessionData
+        {
+            public int User_ID { get; set; }
+            public string User_Name { get; set; }
+            public string User_LoginID { get; set; }
+            public int User_Role { get; set; }
+        }
+        public class SearchViewModel
+        {
+            public string StartDate { get; set; }
+            public string EndDate { get; set; }
+            public string UserID { get; set; }
+        }
+
+        public class LoginViewModel
+        { 
+        [Required, AllowHtml]
+        public string Username { get; set; }
+
+        [Required]
+        [AllowHtml]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        }
+        public class QuizModel
+        {
+            public int UserId { get; set; }
+            public int QuizID { get; set; }
+            public int TimeAllocated { get; set; }
+        }
+        public class QuestionsModel
+        {
+            public int QuizID { get; set; }
+            public int SubjectID { get; set; }
+            public string SubjectName { get; set; }
+            public string Question { get; set; }
+            public string Option1 { get; set; }
+            public string Option2 { get; set; }
+            public string Option3 { get; set; }
+            public string Option4 { get; set; }
+            public string Answer { get; set; }
+            public string ChoiceType { get; set; }
+
+        }
+    }
+}
